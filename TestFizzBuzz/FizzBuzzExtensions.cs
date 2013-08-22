a210bfb5f2318419683640fa664680a6ebc25ec4
Carles Company Soler <carlescs@gmail.com>
2013-08-22 13:13:21 +0200

    Initial import.

---
+++ TestFizzBuzz/FizzBuzzExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestFizzBuzz
+{
+    public static class FizzBuzzExtensions
+    {
+        public static IEnumerable<string> FizzBuzz<T>(this IEnumerable<T> enumerable, Predicate<T> testFizz, Predicate<T> testBuzz)
+        {
+            return enumerable.Select(i => FizzBuzz(i, testFizz, testBuzz));
+        }
+
+        public static string FizzBuzz<T>(this T i, Predicate<T> testFizz, Predicate<T> testBuzz)
+        {
+            if (!testFizz.Invoke(i) && !testBuzz.Invoke(i))
+                return string.Format("{0}", i);
+            var sb = new StringBuilder();
+            if (testFizz.Invoke(i))
+                sb.Append("Fizz");
+            if (testBuzz.Invoke(i))
+                sb.Append("Buzz");
+            return sb.ToString();
+        }
+    }
+}
---
+++ TestFizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TestFizzBuzz
+{
+    class Program
+    {
+        static void Main()
+        {
+            foreach (var s in Enumerable.Range(1, 20).FizzBuzz(t => t % 3 == 0, t => t % 5 == 0))
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey(true);
+            foreach (var s in Enumerable.Range(1,20))
+            {
+                Console.WriteLine(s.FizzBuzz(t => t%3 == 0, t => t%5 == 0));
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
