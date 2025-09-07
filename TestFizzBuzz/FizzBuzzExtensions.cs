b4af9f4b4d48e5f08cfd3c7fa2726ed26e4a11c6
Carles Company Soler <carles.cs@outlook.com>
2025-09-07 14:18:55 +0200

    feat(FizzBuzzExtensions): enhance FizzBuzz operations
    Updated the `FizzBuzzExtensions` class to allow explicit labels for "Fizz" and "Buzz" in FizzBuzz operations. Introduced a new overload of the `FizzBuzz` method that utilizes standard divisibility predicates (3 and 5) without requiring labels.
    
    Documentation comments have been revised to clarify the purpose and parameters of each method. ✨

--- TestFizzBuzz/FizzBuzzExtensions.cs
+++ TestFizzBuzz/FizzBuzzExtensions.cs
@@ -11,41 +11,64 @@ namespace TestFizzBuzz;
 public static class FizzBuzzExtensions
 {
     /// <summary>
-    /// Performs FizzBuzz operation on the elements of the collection based on the provided predicates.
+    /// Performs FizzBuzz operation on the elements of the collection using the default Fizz and Buzz predicates.
     /// </summary>
     /// <typeparam name="T">The type of elements in the collection.</typeparam>
     /// <param name="enumerable">The collection to perform FizzBuzz operation on.</param>
     /// <param name="testFizz">The predicate to test if an element should be replaced with the fizz label.</param>
     /// <param name="testBuzz">The predicate to test if an element should be replaced with the buzz label.</param>
-    /// <param name="fizzLabel">Label used when the fizz predicate matches. Defaults to "Fizz".</param>
-    /// <param name="buzzLabel">Label used when the buzz predicate matches. Defaults to "Buzz".</param>
     /// <returns>An enumerable of strings representing the FizzBuzz operation results.</returns>
-    public static IEnumerable<string> FizzBuzz<T>(this IEnumerable<T> enumerable, Predicate<T> testFizz, Predicate<T> testBuzz, string fizzLabel = "Fizz", string buzzLabel = "Buzz")
+    public static IEnumerable<string> FizzBuzz<T>(this IEnumerable<T> enumerable, Predicate<T> testFizz, Predicate<T> testBuzz)
+    {
+        return enumerable.FizzBuzz(testFizz, testBuzz, "Fizz", "Buzz");
+    }
+
+    /// <summary>
+    /// Performs FizzBuzz operation on the elements of the collection based on the provided predicates and explicit labels.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="enumerable">The collection to perform FizzBuzz operation on.</param>
+    /// <param name="testFizz">The predicate to test if an element should be replaced with the fizz label.</param>
+    /// <param name="testBuzz">The predicate to test if an element should be replaced with the buzz label.</param>
+    /// <param name="fizzLabel">Label used when the fizz predicate matches.</param>
+    /// <param name="buzzLabel">Label used when the buzz predicate matches.</param>
+    /// <returns>An enumerable of strings representing the FizzBuzz operation results.</returns>
+    public static IEnumerable<string> FizzBuzz<T>(this IEnumerable<T> enumerable, Predicate<T> testFizz, Predicate<T> testBuzz, string fizzLabel, string buzzLabel)
     {
         return enumerable.Select(i => FizzBuzzElem(i, testFizz, testBuzz, fizzLabel, buzzLabel));
     }
 
     /// <summary>
-    /// Performs FizzBuzz operation on the elements of the collection using the default Fizz and Buzz predicates.
+    /// Performs FizzBuzz operation on the elements of the collection using the standard divisibility predicates (3 and 5).
     /// </summary>
     /// <param name="enumerable">The collection to perform FizzBuzz operation on.</param>
-    /// <param name="fizzLabel">Label used when the fizz predicate matches. Defaults to "Fizz".</param>
-    /// <param name="buzzLabel">Label used when the buzz predicate matches. Defaults to "Buzz".</param>
     /// <returns>An enumerable of strings representing the FizzBuzz operation results.</returns>
-    public static IEnumerable<string> FizzBuzz(this IEnumerable<int> enumerable, string fizzLabel = "Fizz", string buzzLabel = "Buzz")
+    public static IEnumerable<string> FizzBuzz(this IEnumerable<int> enumerable)
+    {
+        return enumerable.FizzBuzz(t => t % 3 == 0, t => t % 5 == 0);
+    }
+
+    /// <summary>
+    /// Performs FizzBuzz operation on the elements of the collection using the standard divisibility predicates (3 and 5) with explicit labels.
+    /// </summary>
+    /// <param name="enumerable">The collection to perform FizzBuzz operation on.</param>
+    /// <param name="fizzLabel">Label used when the fizz predicate matches.</param>
+    /// <param name="buzzLabel">Label used when the buzz predicate matches.</param>
+    /// <returns>An enumerable of strings representing the FizzBuzz operation results.</returns>
+    public static IEnumerable<string> FizzBuzz(this IEnumerable<int> enumerable, string fizzLabel, string buzzLabel)
     {
         return enumerable.FizzBuzz(t => t % 3 == 0, t => t % 5 == 0, fizzLabel, buzzLabel);
     }
 
     /// <summary>
-    /// Performs FizzBuzz operation on a single element based on the provided predicates.
+    /// Performs FizzBuzz operation on a single element based on the provided predicates and labels.
     /// </summary>
     /// <typeparam name="T">The type of the element.</typeparam>
     /// <param name="i">The element to perform FizzBuzz operation on.</param>
     /// <param name="testFizz">The predicate to test if the element should be replaced with the fizz label.</param>
     /// <param name="testBuzz">The predicate to test if the element should be replaced with the buzz label.</param>
-    /// <param name="fizzLabel">Label used when the fizz predicate matches. Defaults to "Fizz".</param>
-    /// <param name="buzzLabel">Label used when the buzz predicate matches. Defaults to "Buzz".</param>
+    /// <param name="fizzLabel">Label used when the fizz predicate matches.</param>
+    /// <param name="buzzLabel">Label used when the buzz predicate matches.</param>
     /// <returns>A string representing the FizzBuzz operation result for the element.</returns>
     private static string FizzBuzzElem<T>(this T i, Predicate<T> testFizz, Predicate<T> testBuzz, string fizzLabel, string buzzLabel)
     {
